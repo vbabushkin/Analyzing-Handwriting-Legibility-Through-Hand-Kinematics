dcd9bf1c432e97005c7c2e89fd779342f12eb3e1
Vahan Babushkin <vb1121@nyu.edu>
2025-02-09 17:18:31 +0400

    del

--- LEGIBILITY_STUDY/preprocess_data.py
+++
@@ -1,9 +0,0 @@
-import utilities
-
-def preprocess_data():
-    utilities.preprocess()
-
-if __name__ == "__main__":
-    print("Usage: python preprocess_data.py")
-    preprocess_data()
-    print("Data preprocessing complete.")
